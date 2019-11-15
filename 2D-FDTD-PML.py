497dec7e82168da0917641fe084a8b3a933188b5
Patrick Cook <pdcook@mail.fhsu.edu>
2019-11-14 20:44:28 -0600

    formatting

--- 2D-FDTD-PML.py
+++ 2D-FDTD-PML.py
@@ -16,10 +16,11 @@ def FDTD2D(media,pulse,Nt,PMLSize = 5):
         electric and magnetic fields forward in time.
 
         Returns:
-            Ez - electric field in the z direction as a function of time and space
-            Dz - electric flux in the z direction as a function of time and space
-            Hx - magnetic field in the x direction as a function of time and space
-            Hy - magnetic field in the y direction as a function of time and space
+         Ez - electric field in the z direction as a function of time and space
+         Dz - electric displacement in the z direction as a function of time
+                and space
+         Hx - magnetic field in the x direction as a function of time and space
+         Hy - magnetic field in the y direction as a function of time and space
     """
 
     Ny, Nx = media.shape
@@ -30,8 +31,9 @@ def FDTD2D(media,pulse,Nt,PMLSize = 5):
 
     times = np.arange(0,Nt*dt,dt)
 
-    # all arrays are indexed as [ti,yi,xi] (unless there is no t component, in which case they are indexed as [yi,xi])
-    Dz  = np.zeros((Nt,Ny,Nx)) # flux density
+    # all arrays are indexed as [ti,yi,xi] (unless there is no t component, in
+    #   which case they are indexed as [yi,xi])
+    Dz  = np.zeros((Nt,Ny,Nx)) # electric displacement
     Ez  = np.zeros((Nt,Ny,Nx)) # electric field
     Hx  = np.zeros((Nt,Ny,Nx)) # magnetic field
     Hy  = np.zeros((Nt,Ny,Nx)) # magnetic field
@@ -88,10 +90,14 @@ def FDTD2D(media,pulse,Nt,PMLSize = 5):
     for ti, t in enumerate(times):
         if ti == 0: continue
         # I use a numpy trick here to do h[ti][yi][xi-1], by using np.roll
-        # axis = 0 corresponds to the y axis and axis = 1 corresponds to the x axis
+        # axis = 0 corresponds to the y axis and axis = 1
+        #   corresponds to the x axis
 
-        # calculate flux density                                                                                               # add pulse #
-        Dz[ti] = gx3*gy3*Dz[ti-1]+0.5*gx2*gy2*(Hy[ti-1] - np.roll(Hy[ti-1],-1,axis=1) - Hx[ti-1] + np.roll(Hx[ti-1],-1,axis=0)) + Pz[ti]
+        # calculate electric displacement
+        Dz[ti] = gx3*gy3*Dz[ti-1]+0.5*gx2*gy2*(Hy[ti-1] - \
+            np.roll(Hy[ti-1],-1,axis=1) - Hx[ti-1] + \
+            np.roll(Hx[ti-1],-1,axis=0)) \
+            + Pz[ti]    # add pulse
 
         # calculate electric field
         Ez[ti] = Gaz*(Dz[ti]-Iz[ti-1])
@@ -162,11 +168,12 @@ class Media:
 
 class Pulse:
     """
-        The Pulse class defines a pulse to set as the electric flux in the simulation.
+        The Pulse class defines a pulse to set as the electric displacement
+            in the simulation.
         It creates a 2D gaussian pulse in both time and space
         It is definted by:
-            center          - where the pulse will be centered in space, (x0,y0)
-            radial_center   - where the pulse will peak radially from the center
+            center        - where the pulse will be centered in space, (x0,y0)
+            radial_center - where the pulse will peak radially from the center
                                 for example:
                                 radial_center = 0:          radial_center > 0:
 
@@ -184,7 +191,8 @@ class Pulse:
             temporal_spread - the standard deviation of the gaussian in time
 
     """
-    def __init__(self, media, center, radial_center, spacial_spread, temporal_center, temporal_spread):
+    def __init__(self, media, center, radial_center, spacial_spread, \
+                     temporal_center, temporal_spread):
 
         self.temporal_center = temporal_center
         self.temporal_spread = temporal_spread
@@ -193,18 +201,22 @@ class Pulse:
         dx     = media.dx
         x0, y0 = center
 
-        x, y    = np.meshgrid(np.linspace(-Nx/2,Nx/2,Nx)*dx, np.linspace(-Ny/2,Ny/2,Ny)*dx)
+        x, y    = np.meshgrid(np.linspace(-Nx/2,Nx/2,Nx)*dx, \
+                    np.linspace(-Ny/2,Ny/2,Ny)*dx)
         d       = np.sqrt((x-x0)**2+(y-y0)**2)
-        self.g_space = np.exp(-( (( d - radial_center )**2) / ( 2.0 * spacial_spread**2 ) ) )
+        self.g_space = np.exp(-( (( d - radial_center )**2) / \
+                    ( 2.0 * spacial_spread**2 ) ) )
 
     def getPulse(self, times):
         """
             The getPulse method generates the pulse at the supplied times.
         """
 
-        g_time = np.exp(-(times-self.temporal_center)**2 / (2 * self.temporal_spread**2) )
+        g_time = np.exp(-(times-self.temporal_center)**2 / \
+                    (2 * self.temporal_spread**2) )
 
-        return (g_time*(self.g_space*np.ones((g_time.size,self.g_space.shape[0],self.g_space.shape[1]))).T).T
+        return (g_time*(self.g_space*np.ones((g_time.size,\
+                    self.g_space.shape[0],self.g_space.shape[1]))).T).T
 
 def Animate(F, excludePML = 0, cmin = None, cmax = None):
     """
@@ -239,10 +251,11 @@ def Energy(Ez,Hx,Hy):
     E_Hy = np.sum(Hy**2, axis=(1,2))
     return E_Ez+E_Hx+E_Hy
 
-################################################################################
+###############################################################################
 
 media = Media(200,200,0.1)
-pulse = Pulse(media, (0*media.dx,-25*media.dx), 10*media.dx, 10*media.dx, 10*media.dt, 2*media.dt)
+pulse = Pulse(media, (0*media.dx,-25*media.dx), \
+             10*media.dx, 10*media.dx, 10*media.dt, 2*media.dt)
 Ez, Dz, Hx, Hy = FDTD2D(media, pulse, 200, PMLSize = 10)
 E = Energy(Ez,Hx,Hy)
 Animate(Ez)
